b2a32e950572755845f287fd392e341d6e3fb023
Abdumannof <129966492+Zaxleb@users.noreply.github.com>
2023-08-27 11:53:49 +0300

    #fix #diretion

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <React.Fragment>
       <div>
         <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus praesentium cum excepturi! Sapiente quod dolore, eos optio provident quos accusantium illo deserunt, repudiandae voluptatibus quasi veritatis dignissimos voluptate distinctio dicta!</p>
       </div>
     </React.Fragment>
   );
